619ddc1e54cf483807970c0fff5be51a80bf99fc
ovkachanov <ovkachanov1988@gmail.com>
2018-09-05 15:30:20 +0300

    hw3 fix

--- movie.rb
+++ movie.rb
@@ -1,3 +1,4 @@
+require "pp"
 HEADERS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
 
 file_path = './data/movies.txt'
@@ -16,19 +17,19 @@ end
 puts "================================================="
 
 the_longests = films.sort_by { |film| film[:duration].to_i }.reverse.take(5)
-the_longests.each { |value| puts "#{value[:name]} #{value[:duration]}" }
+the_longests.each { |value| puts "#{value[:name]} (#{value[:date]}; #{value[:genre]}) - #{value[:duration]}" }
 
 puts "================================================="
 
 comedy = films.select { |value| value[:genre].include?('Comedy')}.sort_by{ |value| value[:date] }
-comedy.each { |value| puts "#{value[:name]} Release date: #{value[:date]}" }
+comedy.each { |value| puts "#{value[:name]} (#{value[:date]}; #{value[:genre]}) - #{value[:duration]}" }
 
 puts "================================================="
 
-authors = films.sort_by { |value| value[:author] }.uniq { |value| value[:author] }
+authors = films.sort_by { |value| value[:author].split(" ").last }.uniq { |value| value[:author] }
 authors.each { |value| puts value[:author] }
 
 puts "================================================="
 
-movies_shot_not_us = films.select { |value| value[:country] != 'USA' }
-puts movies_shot_not_us.count
+movies_shot_not_us = films.count { |value| value[:country] != 'USA' }
+puts movies_shot_not_us
